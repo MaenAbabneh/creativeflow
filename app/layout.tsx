d95bc71e47095ea48a550b52af789379fac53ca8
Maen <maenababneh@outlook.com>
2025-05-22 18:32:04 +0300

    modify puplic file ,font and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,79 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono as GeistMono  } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/Interfont.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const Grotisk = localFont({
+  src: "./fonts/SpaceGrotesk.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
 });
 
-export const metadata: Metadata = {
-  title: "creative-overflow",
-  description: "the best place to find creative inspiration",
+const Tajwal = localFont({
+  src: [
+    {
+      path: "./fonts/Tajawal-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Tajawal-Medium.ttf",
+      style: "normal",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Tajawal-Regular.ttf",
+      style: "normal",
+      weight: "400",
+    },
+  ],
+  variable: "--font-tajwal",
+});
+
+export const metadata: { [key: string]: Metadata } = {
+  en: {
+    title: "Creative Overflow",
+    description:
+      "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    openGraph: {
+      title: "Creative Overflow",
+      description:
+        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    },
+    twitter: {
+      title: "Creative Overflow",
+      description:
+        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    },
+  },
+
+  ar: {
+    title: "Creative Overflow",
+    description:
+      "منصة مدفوعة بالمجتمع لطرح الأسئلة والإجابة عليها في مجال البرمجة. احصل على المساعدة، وشارك المعرفة، وتعاون مع المطورين من جميع أنحاء العالم. استكشف مواضيع في تطوير الويب، وتطوير تطبيقات الهواتف المحمولة، والخوارزميات، وهياكل البيانات، والمزيد.",
+    openGraph: {
+      title: "Creative Overflow",
+      description:
+        "منصة مدفوعة بالمجتمع لطرح الأسئلة والإجابة عليها في مجال البرمجة. احصل على المساعدة، وشارك المعرفة، وتعاون مع المطورين من جميع أنحاء العالم. استكشف مواضيع في تطوير الويب، وتطوير تطبيقات الهواتف المحمولة، والخوارزميات، وهياكل البيانات، والمزيد.",
+    },
+    twitter: {
+      title: "Creative Overflow",
+      description:
+        "منصة مدفوعة بالمجتمع لطرح الأسئلة والإجابة عليها في مجال البرمجة. احصل على المساعدة، وشارك المعرفة، وتعاون مع المطورين من جميع أنحاء العالم. استكشف مواضيع في تطوير الويب، وتطوير تطبيقات الهواتف المحمولة، والخوارزميات، وهياكل البيانات، والمزيد.",
+    },
+  },
+
+  icons: {
+    icons: 
+    {
+      icon:'images/site-icon.png',
+    },
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +84,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${Grotisk.variable} ${Tajwal.variable} font-sans antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <h1>welcome to my first project</h1>
+    <>
+      <h1 className="h1-bold font-space-grotesk font-bold">
+        welcome to my first project
+      </h1>
+      <h1 className="font-inter size text-2xl  ">welcome to my first project</h1>
+      <h1 className=" font-tajwal">اهلا في اول مشروع لي</h1>
+    </>
   );
 }
+ 
