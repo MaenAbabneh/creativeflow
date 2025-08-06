6627f27398fbca42743a1758051a7fdf557be256
Maen <maenababneh@outlook.com>
2025-08-06 17:59:33 +0300

    feat: Enhance metadata for SEO and social sharing, update icons, and improve Open Graph settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,15 +39,68 @@ const Tajwal = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Creative Overflow",
+  generator: "Next.js",
+  applicationName: "Creative Overflow",
+  referrer: "origin-when-cross-origin",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  keywords: ["programming", "coding", "developer community"],
+  keywords: ["programming", "coding", "developer community" , "Q&A", "web development", "mobile app development", "algorithms", "data structures"],
   authors: [{ name: "Maen Ababenh" }],
-  icons: {
-    icon: "images/site-icon.png",
+  creator: "Maen Ababenh",
+  publisher: "Maen Ababenh",
+
+  openGraph: {
+    title: "Creative Overflow",
+    description:
+      "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world.",
+    url: "https://creativeoverflow.dev",
+    siteName: "Creative Overflow",
+    images: [
+      {
+        url: "https://creativeoverflow.dev/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Creative Overflow - A community-driven platform for programming questions",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
+   formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
+  icons: {
+    icon: "/images/site-logo.svg", // regular favicon
+    shortcut: "/favicon.ico", // browser address bar icon
+    apple: "/apple-touch-icon.png", // Apple devices
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
+
 };
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -55,7 +108,7 @@ export default async function RootLayout({
 }>) {
   const session = await auth();
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" >
       <head>
         <link
           rel="stylesheet"
