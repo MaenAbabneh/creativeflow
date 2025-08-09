d8530749000ef723473df3bd0bb830480f9f8125
Maen <maenababneh@outlook.com>
2025-08-09 13:59:37 +0300

    feat: implement Logo component for mobile and desktop views in Navbar

--- components/navigation/navbar/navbar.tsx
+++ components/navigation/navbar/navbar.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 import { auth } from "@/auth";
-import MobileRightSidebar from "@/components/navigation/navbar/mobile-right-sidebar"; // New: Right mobile sidebar
-import MobileSidebar from "@/components/navigation/navbar/mobile-sidebar"; // Left mobile sidebar
 import GlobalSearch from "@/components/search/globale-search";
 import UserAvatar from "@/components/UserAvatar";
 
+import Logo from "./logo";
+import MobileRightSidebar from "./mobile-right-sidebar";
+import MobileSidebar from "./mobile-sidebar";
 import Theme from "./theme";
 
 const Navbar = async () => {
@@ -17,37 +16,19 @@ const Navbar = async () => {
     <nav className="flex-between background-light900_dark300 fixed w-full z-50 gap-5 p-4  shadow-light-100 dark:shadow-none">
       {/* Left Section: Logo & Mobile Left Sidebar Trigger */}
       <div className="flex items-center gap-3">
-        <Link href="/" className="items-center gap-1.5 sm:hidden">
-          <Image
-            src="/images/site-logo.svg"
-            alt="Creative Overflow Logo"
-            width={35}
-            height={35}
-          />
-        </Link>
-        <Link href="/" className="items-center gap-1.5 hidden sm:flex">
-          <Image
-            src="/images/Logo-dark.svg"
-            alt="Creative Overflow Logo Dark"
-            width={200}
-            height={40}
-            className="block dark:hidden object-contain"
-          />
-          <Image
-            src="/images/Logo-light.svg"
-            alt="Creative Overflow Logo Light"
-            width={200}
-            height={40}
-            className="hidden dark:block object-contain"
-          />
-        </Link>
+        {/* Mobile Logo */}
+        <Logo isMobile />
+
+        {/* Desktop Logo */}
+        <Logo />
+
         {/* Mobile Left Sidebar Trigger (SheetTrigger is inside MobileSidebar) */}
         <MobileSidebar />
       </div>
 
       {/* Center Section: Search Bar (visible on sm and up) */}
       <div className="hidden sm:flex items-center gap-2 flex-1 mx-4 max-w-md lg:max-w-xl light-border-2 background-light700_dark300 rounded-lg px-3 py-2 shadow-light-100 dark:shadow-none">
-      <GlobalSearch />
+        <GlobalSearch />
       </div>
 
       {/* Right Section: Theme Toggle & Mobile Right Sidebar Trigger */}
